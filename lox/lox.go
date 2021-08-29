3c4b19bd12a12446bdd364ccabc1b0cf4123a920
singurty <singurty@gmail.com>
2021-08-29 20:28:52 +0545

    define token enums

--- lox/lox.go
+++ lox/lox.go
@@ -1,5 +1,55 @@
 package lox
 
+const (
+	// Single-character tokens
+	LEFT_PAREN = iota
+	RIGHT_PAREN
+	LEFT_BRACE
+	RIGHT_BRACE
+	COMMA
+	DOT
+	MINUS
+	PLUS
+	SEMICOLON
+	SLASH
+	STAR
+
+	// One or two character tokens
+	BANG
+	BANG_EQUAL
+	EQUAL
+	EQUAL_EQUAL
+	GREATER
+	GREATER_EQUAL
+	LESS
+	LESS_EQUAL
+
+	// Literals
+	IDENTIFIER
+	STRING
+	NUMBER
+
+	// Keywords
+	AND
+	CLASS
+	ELSE
+	FALSE
+	FUN
+	FOR
+	IF
+	NIL
+	OR
+	PRINT
+	RETURN
+	SUPER
+	THIS
+	TRUE
+	VAR
+	WHILE
+
+	EOF
+)
+
 func Run(source string) {
 
 }
